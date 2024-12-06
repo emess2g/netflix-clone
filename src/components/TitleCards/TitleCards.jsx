df606704968b5dc7b276cebcb910c5e536fe84f6
emess2g <essuonemma84gmail.com>
2024-12-06 09:39:57 +0000

    movies categories

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef,  } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
 
 const cardsRef = useRef();
 
@@ -12,12 +12,12 @@ const handleWheel = (event) => {
 }
 
 useEffect(()=>{
-  cardsRef.current.addEventListener('wheel', handleWheel)
+  cardsRef.current.addEventListener('wheel', handleWheel);
 },[])
 
   return (
     <div className='title-cards' >
-      <h2>Popular on Netflix</h2>
+      <h2>{title? title : 'Popular on Netflix'}</h2>
       <div className="card-list" ref={cardsRef}>
         {cards_data.map((card,index)=>{
           return <div className="card" key={index}>
