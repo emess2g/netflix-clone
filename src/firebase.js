40e7b6c0b4d7165bab446add67f82759f0efdbab
emess2g <essuonemma84gmail.com>
2024-12-07 16:59:21 +0000

    mobile device ui

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { addDoc, collection, getFirestore} from 'firebase/firestore'
+import { toast } from "react-toastify";
 
 
 const firebaseConfig = {
@@ -33,7 +34,7 @@ const signup = async (name, email, password) => {
         })
     } catch (error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code.split('/')[1].split('-').join(' '));
     }
 }
 
@@ -44,7 +45,7 @@ const login = async(email,password) => {
          await signInWithEmailAndPassword(auth, email,password)
     } catch (error) {
         console.log(error);
-        alert(error)
+        toast.error(error.code.split('/')[1].split('-').join(' '));
     }
 }
 
